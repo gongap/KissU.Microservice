230d1e3df7e459b39778128feaab18365aaab4a7
gongap <gonganping@qq.com>
2020-08-22 16:47:34 +0800

    RunMigrations

--- src/KissU.DbMigrator/Program.cs
+++ src/KissU.DbMigrator/Program.cs
@@ -1,7 +1,7 @@
+using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 
@@ -25,23 +25,49 @@ namespace KissU.DbMigrator
                 .WriteTo.Console()
                 .CreateLogger();
 
-            await CreateHostBuilder(args).RunConsoleAsync();
+            Console.WriteLine("Initializing KissU DbMigrator ... ");
+            await RunMigrations();
+            Console.WriteLine("\n\nPress ENTER to exit ...");
+            Console.ReadLine();
         }
 
-        internal static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
+        private static async Task RunMigrations()
+        {
+            Console.Write("\nThis program updates an existing database or creates a new one if not exists.\n" +
+                          "Are you sure you want to run the migration? (y/n) ");
+
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.WriteLine("\n\nMigrating database...");
+
+                try
                 {
-                    services.AddHostedService<Modules.Blogging.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.Identity.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.IdentityServer.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.AuditLogging.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.BackgroundJobs.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.TenantManagement.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.SettingManagement.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.FeatureManagement.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<Modules.PermissionManagement.DbMigrator.DbMigratorHostedService>();
-                    services.AddHostedService<AppHostedService>();
-                });
+                    var cts = new CancellationTokenSource();
+                    await new Modules.Blogging.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.Identity.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.IdentityServer.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.AuditLogging.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.BackgroundJobs.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.TenantManagement.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.SettingManagement.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.FeatureManagement.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new Modules.PermissionManagement.DbMigrator.DbMigratorHostedService().StartAsync(cts.Token);
+                    await new AppHostedService().StartAsync(cts.Token);
+                    Console.WriteLine("Migration completed.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    while (ex.InnerException != null)
+                    {
+                        ex = ex.InnerException;
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    Console.Write("\nThere was problem while applying migrations. " );
+                }
+            }
+        }
     }
 }
