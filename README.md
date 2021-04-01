ef8cd3334a75518a1344e10c7d3a3cc0fb376ca5
龚安平 <gonganping@qq.com>
2021-04-01 09:55:43 +0800

    重构KissU.Microservice

--- README.md
+++ README.md
@@ -9,4 +9,3 @@ Sample solutions built with the KissU Framework
 ### Pull Requests
 
 * You can send PRs to this repository.
-* When you create a new project, please update the [`common.ps1`](build/common.ps1) file.
---
+++ modules/identity/KissU.Modules.Identity.Service/Implements/ProfileService.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using KissU.Modules.Identity.Service.Contracts;
+using KissU.ServiceProxy;
+using Volo.Abp.Identity;
+
+namespace KissU.Modules.Identity.Service.Implements
+{
+    /// <summary>
+    /// 用户信息
+    /// Implements the <see cref="ProxyServiceBase" />
+    /// Implements the <see cref="IProfileService" />
+    /// </summary>
+    /// <seealso cref="ProxyServiceBase" />
+    /// <seealso cref="IProfileService" />
+    public class ProfileService : ProxyServiceBase, IProfileService
+    {
+        private readonly IProfileAppService _appService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileService"/> class.
+        /// </summary>
+        /// <param name="appService">The application service.</param>
+        public ProfileService(IProfileAppService appService)
+        {
+            _appService = appService;
+        }
+
+        /// <summary>
+        /// 获取基本信息
+        /// </summary>
+        /// <returns>Task&lt;ProfileDto&gt;.</returns>
+        public virtual Task<ProfileDto> Get()
+        {
+            return _appService.GetAsync();
+        }
+
+        /// <summary>
+        /// 更新基本信息
+        /// </summary>
+        /// <param name="parameters">请求参数</param>
+        /// <returns>Task&lt;ProfileDto&gt;.</returns>
+        public Task<ProfileDto> Update(UpdateProfileDto parameters)
+        {
+            return _appService.UpdateAsync(parameters);
+        }
+
+        /// <summary>
+        /// 修改密码
+        /// </summary>
+        /// <param name="parameters">请求参数</param>
+        /// <returns>Task.</returns>
+        public async Task ChangePassword(ChangePasswordInput parameters)
+        {
+            await _appService.ChangePasswordAsync(parameters);
+        }
+    }
+}
